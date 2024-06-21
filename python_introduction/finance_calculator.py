f18514d8ef5017a2a57df57333928963b0e4ed17
Ouko Okowa <oukopinto@gmail.com>
2024-06-21 16:52:51 +0200

    Added a new file that contains a script performing yearly projected savings calculation in python

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Calculates user's yearly potential savings based on monthly income and expenses and 5% interest rate
-income = float(input("Enter your monthly income: "))
-expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-savings = float(income - expenses)
-projected_savings = float(savings * 12 + (savings * 12 * 0.05))
+monthly_savings = (monthly_income - monthly_expenses)
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 *  0.05)
 
-print("Your monthly savings are: $"+str(savings))
-print("Projected savings after one year, with interest, is: $"+str(projected_savings))
+print("Your monthly savings are: $"+str(round(monthly_savings, 2)))
+print("Projected savings after one year, with interest, is: $"+str(round(projected_savings, 2)))
