27fc8df827eb612503c1b83e3d6977a17660b31c
Ouko Okowa <oukopinto@gmail.com>
2024-06-26 17:02:12 +0200

    Added a file that contains a script that generates a daily reminder based on priority and the urgency of the task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,16 +6,16 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 match priority:
     case "high":
         if time_bound == "yes":
-            print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!") 
+            print("Reminder:", "'" + task + "'", "is a", priority, "priority task that requires immediate attention today!") 
         else:
-            print(f"Note: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your break time!")
+            print("Note:", "'" + task + "'", "is a", priority, "priority task that is not urgent. Consider completing it during your break time!")
     case "medium":
         if time_bound == "yes":
-            print(f"Reminder: '{task}' is a {priority} priority task that requires your attention before the end of the day!") 
+            print("Reminder:", "'" + task + "'", "is a", priority, "priority task that requires your attention before the end of the day!") 
         else:
-            print(f"Note: '{task}' is a {priority} priority task that is not urgent. Consider completing it sooner than later!")
+            print("Note:", "'" + task + "'", "is a", priority, "priority task that is not urgent. Consider completing it sooner than later!")
     case "low":
         if time_bound == "yes":
-            print(f"Reminder: '{task}' is a {priority} priority task that requires your attention") 
+            print("Reminder:", "'" + task + "'", "is a", priority "priority task that requires your attention") 
         else:
-            print(f"Note: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your free time!")
+            print("Note:", "'" + task + "'", "is a", priority, "priority task that is not urgent. Consider completing it during your free time!")
