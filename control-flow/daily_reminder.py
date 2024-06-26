4e2ec49a0bb20ee2e4a96ff6525a3bfec7e081b9
Ouko Okowa <oukopinto@gmail.com>
2024-06-26 16:42:15 +0200

    Added a file that contains a script that generates a daily reminder based on priority and the urgency of the task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -8,14 +8,14 @@ match priority:
         if time_bound == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!") 
         else:
-            print(f"Reminder: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your break time!")
+            print(f"Note: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your break time!")
     case "medium":
         if time_bound == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires your attention before the end of the day!") 
         else:
-            print(f"Reminder: '{task}' is a {priority} priority task that is not urgent. Consider completing it sooner than later!")
+            print(f"Note: '{task}' is a {priority} priority task that is not urgent. Consider completing it sooner than later!")
     case "low":
         if time_bound == "yes":
-            print(f"Notice: '{task}' is a {priority} priority task that requires your attention") 
+            print(f"Reminder: '{task}' is a {priority} priority task that requires your attention") 
         else:
-            print(f"Notice: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your free time!")
+            print(f"Note: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your free time!")
