442be962c08824915ebcc2fc66015adf7f0fef9f
Ouko Okowa <oukopinto@gmail.com>
2024-06-26 16:04:27 +0200

    Added a file that contains a script that generates a daily reminder based on priority and the urgency of the task

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,22 @@
+#Makes a personal daily reminder based on task, priority, and time-bound or not
+task = input("Enter your task: ")
+task_priority = input("Enter task priority (high/medium/low): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
+
+match task_priority:
+    case "high":
+        if time_bound == "yes":
+            result = f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!" 
+        else:
+            result = f"Reminder: '{task}' is a {task_priority} priority task that is not urgent. Consider completing it during your break time!"
+    case "medium":
+        if time_bound == "yes":
+            result = f"Reminder: '{task}' is a {task_priority} priority task that requires your attention before the end of the day!" 
+        else:
+            result = f"Reminder: '{task}' is a {task_priority} priority task that is not urgent. Consider completing it sooner than later!"
+    case "low":
+        if time_bound == "yes":
+            result = f"Notice: '{task}' is a {task_priority} priority task that requires your attention" 
+        else:
+            result = f"Notice: '{task}' is a {task_priority} priority task that is not urgent. Consider completing it during your free time!"
+print(result)
