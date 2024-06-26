5d70caacaf7dc330c2d08cc001a4aeef1f3330df
Ouko Okowa <oukopinto@gmail.com>
2024-06-26 16:17:50 +0200

    Added a file that contains a script that generates a daily reminder based on priority and the urgency of the task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,20 +3,20 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
 time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-match task_priority:
+match priority:
     case "high":
         if time_bound == "yes":
-            result = f"Reminder: '{task}' is a {task_priority} priority task that requires immediate attention today!" 
+            result = f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!" 
         else:
-            result = f"Reminder: '{task}' is a {task_priority} priority task that is not urgent. Consider completing it during your break time!"
+            result = f"Reminder: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your break time!"
     case "medium":
         if time_bound == "yes":
-            result = f"Reminder: '{task}' is a {task_priority} priority task that requires your attention before the end of the day!" 
+            result = f"Reminder: '{task}' is a {priority} priority task that requires your attention before the end of the day!" 
         else:
-            result = f"Reminder: '{task}' is a {task_priority} priority task that is not urgent. Consider completing it sooner than later!"
+            result = f"Reminder: '{task}' is a {priority} priority task that is not urgent. Consider completing it sooner than later!"
     case "low":
         if time_bound == "yes":
-            result = f"Notice: '{task}' is a {task_priority} priority task that requires your attention" 
+            result = f"Notice: '{task}' is a {priority} priority task that requires your attention" 
         else:
-            result = f"Notice: '{task}' is a {task_priority} priority task that is not urgent. Consider completing it during your free time!"
+            result = f"Notice: '{task}' is a {priority} priority task that is not urgent. Consider completing it during your free time!"
 print(result)
