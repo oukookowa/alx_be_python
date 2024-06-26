7c166f8b19ee206d9a3d6891fc98208bb7745b94
Ouko Okowa <oukopinto@gmail.com>
2024-06-26 11:59:10 +0200

    Added a file that contains a script that does simple calculation based om match case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+imple calculator with match case
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = round(num1 / num2, 2)
+    case _:                                                                                                                     result = "Wrong operation entered."                                                                             print("The result is", result)
