38269a40c292b369d866dd9ed796d7a33282eb3f
Ouko Okowa <oukopinto@gmail.com>
2024-06-27 12:21:36 +0200

    Modified the daily remiminder to comply with the desired alx checker prompts

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,20 +3,20 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
 time_bound = input("Is it time-bound? (yes/no): ").lower()
 
-reminder = "'" + task + "'"
 match priority:
     case "high":
         if time_bound == "yes":
-            print("Reminder:", reminder, "is a", priority, "priority task that requires immediate attention today!") 
+            reminder = f"'{task}' is a high priority task that requires immediate attention today!"
         else:
-            print("Reminder:", reminder, "is a", priority, "priority task that is not urgent. Consider completing it during your break time!")
+            reminder = f"'{task}' is a low priority task that is not urgent. Consider completing it during your break time!"
     case "medium":
         if time_bound == "yes":
-            print("Reminder:", reminder, "is a", priority, "priority task that requires your attention before the end of the day!") 
+            reminder = f"'{task}' is a medium priority task that requires your attention before the end of the day!"
         else:
-            print("Reminder", reminder, "is a", priority, "priority task that is not urgent. Consider completing it sooner than later!")
+            reminder = f"'{task}' is a medium priority task that is not urgent. Consider completing it sooner than later!"
     case "low":
         if time_bound == "yes":
-            print("Reminder:", reminder, "is a", priority, "priority task that requires your attention") 
+            reminder = f"'{task}' is a low priority priority task that requires your attention" 
         else:
-            print("Reminder", reminder, "is a", priority, "priority task that is not urgent. Consider completing it during your free time!")
+            reminder = f"'{task}' is a low priority task that is not urgent. Consider completing it during your free time!"
+print("Reminder:", reminder)
