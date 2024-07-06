dcaf38009f037f1e89f43863ef22b75ba84bdb0d
Ouko Okowa <oukopinto@gmail.com>
2024-07-06 17:45:05 +0200

    Modified the program script on the temperature conversion app

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,45 +2,48 @@
 #Converts temperature to degrees celsius and fahrenheit
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
 #Function converts temperature from fahrenheit to celsius
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
     temprature_celcius = round((fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR, 2)
     print(f"{fahrenheit}°F is {temprature_celcius}°C")
+
 #Function converts temperature from celsius to fahrenheit 
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
     temperature_fahrenheit = round(celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32, 2)
     print(f"{celsius}°C is {temperature_fahrenheit}°F")
-#Validation of user input for valid values and strings
 
+#Validation of user input for valid values and strings
 def operation(): 
     try:
         temperature = float(input("Enter the temperature to convert: "))
     except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
-        return None
+        return None             #when the function executes this returns value as None it stops since no calculation will go through
     temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
     if temperature_unit not in ["C", "F"]:
-        print("Invalid entry. Enter a valid temperature unit (C or F)!")
+        print("Invalid entry. Enter a valid temperature unit (C/F)!")
         return None
     #Calling of the functions based on user's choice
     elif temperature_unit == "C":
         return convert_to_fahrenheit(temperature)
-    elif temperature_unit == "F":
+    else: 
+        temperature_unit == "F"
         return convert_to_celsius(temperature)
-    else:
-        print("You must have entered wrong value or unit for temperature!")
-        return None
 
+#Calling the operation function     
+operation()
 while True:
-    operation()
-    response = input("Do you want to continue? Enter Y for yes or N for no: ").upper()   
-    if response == "Y":
+    response = input("Do you want to continue? Enter Y to continue or Q to quit: ").upper()   
+    if response not in ["Y", "Q"]:
+        print("Invalid choice. Enter Y or Q!")
+        pass
+    elif response == "Q":
+        print("Sad to see you go. Goodbye!")
+        break
+    else: 
+        response == "Y"
         operation()
         pass
-    if response not in ["Y", "N"]:
-        print("Invalid choice. Enter Y or N!")
-        pass
-    if response == "N":
-        break
