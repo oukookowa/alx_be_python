27c4c5a068ce3fbed49e49a7b1119d5ac5e4e219
Ouko Okowa <oukopinto@gmail.com>
2024-07-06 16:37:49 +0200

    Modified the program script on the temperature conversion app

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -18,7 +18,7 @@ def operation():
     try:
         temperature = float(input("Enter the temperature to convert: "))
     except ValueError:
-        print("Invalid temperature. Please enter a numeric value!")
+        print("Invalid temperature. Please enter a numeric value.")
         return None
     temperature_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
     if temperature_unit not in ["C", "F"]:
