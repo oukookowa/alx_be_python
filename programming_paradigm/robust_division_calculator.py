3f03314c06fe61958365a976707ec27026a88a90
Ouko Okowa <oukopinto@gmail.com>
2024-07-12 11:51:17 +0200

    Added robust_division_calculator.py and main.py to test arguments from command line

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,25 @@
+#takes 2 numbers as input from user & divides 1st number by 2nd number
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+    try:
+        denominator = float(denominator)
+    except ValueError:
+        print("Error: Please enter numeric values only.")
+
+    #perform division operation with potential errors
+    try: 
+            result = numerator / denominator
+            print(f"The result of the division is {result}")
+    #handle ZeroDivisionError when dividing by zero
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+    #handles the exception in case invalid input is entered
+    except UnboundLocalError:
+        print("You entered the wrong value(s), cannot proceed operation!")
+    except TypeError:
+        print("Unsupported operand type(s) for division")   
+    finally:
+        return "Division operation complete..."
