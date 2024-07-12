7f5bfd7cc85cdc331e8383f9083a22347a1ae26d
Ouko Okowa <oukopinto@gmail.com>
2024-07-12 12:08:24 +0200

    Added robust_division_calculator.py and main.py to test arguments from command line

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,11 +3,11 @@ def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
     try:
         denominator = float(denominator)
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
 
     #perform division operation with potential errors
     try: 
