bb2ae87f89d1f4e0f70dfba7265adbf7a6e0191f
Ouko Okowa <oukopinto@gmail.com>
2024-07-14 12:51:36 +0200

    Added library_management.py and and a test file for the program

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,4 +1,4 @@
-#Define Book class with 3 attributes, title, author, and _is_checked_out
+#Define Book class with 3 attributes, title, author, and a private list _is_checked_out
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -6,15 +6,19 @@ class Book:
         self._is_checked_out = []
 
     def check_out(self):
-        if self not in self._is_checked_out:
-            self._is_checked_out.append(self)
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
 
     def return_book(self):
-        if self in self._is_checked_out:
-            self._is_checked_out.remove(self)
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
 
     def is_available(self):
-        return self not in self._is_checked_out
+        return not self._is_checked_out
 
 #Define Library class to store instances of book as defined in the Book class
 class Library:
