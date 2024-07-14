02e57c4c3de263c49cb85565dd392cd2ec54ed2b
Ouko Okowa <oukopinto@gmail.com>
2024-07-14 10:52:18 +0200

    Added library_management.py and and a test file main-1.py for testing the program

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,46 @@
+#Define Book class with 3 attributes, title, author, and _is_checked_out
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+#Define Library class to store instances of book as defined in the Book class
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and book.is_available():
+                return book.check_out()
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                return book.return_book()
+        return False
+
+    def list_available_books(self):
+        for book in self._books:
+            if book.is_available():
+                print(f"{book.title} by {book.author}")
